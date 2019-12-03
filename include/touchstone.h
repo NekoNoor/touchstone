6094597350aedb0c12d109b2b2d2f244b39c156c
Vincent <>
2019-12-03 19:32:08 +0100

    feat: string assertion

--- include/touchstone.h
+++ include/touchstone.h
@@ -3,15 +3,17 @@
 /*                                                        ::::::::            */
 /*   touchstone.h                                       :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*   By: vincent <vvissche@student.codam.nl>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/11/26 14:22:53 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/30 23:33:08 by nschat        ########   odam.nl         */
+/*   Created: 2019/11/26 14:22:53 by nschat         #+#    #+#                */
+/*   Updated: 2019/12/03 19:26:52 by vincent       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOUCHSTONE_H
 # define TOUCHSTONE_H
+
+# include <string.h>
 
 # define FAIL_MESSAGE "\x1B[97m[\x1B[91mFAIL\x1B[97m]\x1B[0m \x1B[93m"
 # define PASS_MESSAGE "\x1B[97m[\x1B[92mPASS\x1B[97m]\x1B[0m \x1B[93m"
@@ -35,6 +37,11 @@
 	ts_pass_eq(__FILE__, __LINE__, #result, #expected)
 # define fail_eq(result, expected) \
 	ts_fail_eq(__FILE__, __LINE__, #result, #expected)
+
+# define ts_assert_str_eq(result, expected) \
+	(strcmp(result, expected) == 0) ? \
+	pass_eq(result, expected) : \
+	fail_eq(result, expected)
 
 void	ts_pass(char *file, unsigned line, char *result);
 void	ts_fail(char *file, unsigned line, char *result);
--- tests/test.c
+++ tests/test.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   test.c                                             :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*   By: vincent <vvissche@student.codam.nl>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/11/30 20:51:14 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/30 22:30:32 by nschat        ########   odam.nl         */
+/*   Created: 2019/11/30 20:51:14 by nschat         #+#    #+#                */
+/*   Updated: 2019/12/03 19:27:46 by vincent       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ int	main(void)
 	ts_assert(1);
 	ts_assert_eq(ft_atoi("123"), atoi("123"));
 	ts_assert_eq(ft_atoi("123"), atoi("124"));
+	ts_assert_str_eq("string", "string");
+	ts_assert_str_eq("string", "bring");
 }
