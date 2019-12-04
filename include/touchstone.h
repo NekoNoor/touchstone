2b3e83efeb0742219643b2fa2b4ab984763bb93b
Noor Schat <nschat@student.codam.nl>
2019-12-04 17:28:10 +0100

    feat: use ft_strcmp instead of libc strcmp

--- include/touchstone.h
+++ include/touchstone.h
@@ -6,14 +6,13 @@
 /*   By: vvissche <vvissche@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/26 14:22:53 by nschat         #+#    #+#                */
-/*   Updated: 2019/12/04 15:57:21 by nschat        ########   odam.nl         */
+/*   Updated: 2019/12/04 17:25:35 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOUCHSTONE_H
 # define TOUCHSTONE_H
-
-# include <string.h>
+# include "libft.h"
 
 # define FAIL_MESSAGE "\x1B[97m[\x1B[91mFAIL\x1B[97m]\x1B[0m \x1B[93m"
 # define PASS_MESSAGE "\x1B[97m[\x1B[92mPASS\x1B[97m]\x1B[0m \x1B[93m"
@@ -28,7 +27,7 @@
 	ts_result_eq(__FILE__, __LINE__, (result == expected), #result, #expected)
 
 # define ts_assert_str_eq(result, expected) \
-	ts_result_eq(__FILE__, __LINE__, strcmp(result, expected) == 0, #result, #expected)
+	ts_result_eq(__FILE__, __LINE__, ft_strcmp(result, expected) == 0, #result, #expected)
 
 void	ts_result(char *file, unsigned line, size_t expr, char *result);
 void	ts_result_eq(char *file, unsigned line, size_t expr, char *result, char *expected);
